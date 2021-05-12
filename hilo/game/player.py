298290cbb41f138d108955f49cd3e341aec2b6da
Pnzrbj0rn <pnzrbjorn@gmail.com>
2021-05-12 14:54:20 -0600

    Adding the _init_ method and the notes for the player class.

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,3 +1,19 @@
 class Player:
+    """A class for the player. This class of objects is responsible for
+    keeping track of the points and turns.
+    
+    Attributes:
+        draw_card (number): returns a card number and removes it from the list of values.
+        player (Player): An instance of the class of objects known as Player.
+    """
+    
     def __init__(self):
-        pass
+        """The class constructor.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
+        
+        self.cards = []
+        self.draw_card = 0
+        self.get_points = 0
